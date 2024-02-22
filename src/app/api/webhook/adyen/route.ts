a6f8f826808e8cf2258b7add58aaf00d14585eaf
mateudev <mateuszbogacz.dev@gmail.com>
2024-02-22 21:42:14 +0100

    test: webhook adyen

---
+++ src/app/api/webhook/adyen/route.ts
@@ -0,0 +1,15 @@
+import { hmacValidator } from "@adyen/api-library";
+
+export async function GET(request: Request) {
+  const hmacKey = process.env.HMAC_VERIFY_KEY ?? "";
+  const validator = new hmacValidator();
+
+  const signature = request.headers.get("adyen-signature");
+
+  console.log("signature", signature);
+  console.log("validator", validator);
+  console.log("hmacKey", hmacKey);
+
+  console.log("request", request);
+  return Response.json({ status: 200 });
+}
