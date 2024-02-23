6fe99412c49c02980426dca70f1e7facfa9e8c78
mateudev <mateuszbogacz.dev@gmail.com>
2024-02-23 14:38:37 +0100

    fix: api key

--- src/api/adyen/client.ts
+++ src/api/adyen/client.ts
@@ -1,6 +1,8 @@
 import { Client } from "@adyen/api-library";
 
 export const clientAdyen = new Client({
-  apiKey: process.env.API_KEY_ADYEN ?? "test_EXV25GBNCZDHRCZSGIZG7EORRMMACP46",
+  apiKey:
+    process.env.API_KEY_ADYEN ??
+    "AQEwhmfuXNWTK0Qc+iSdi1csqPaeZo5VGcIeeWFPw3bzyzXG7MU4HzpiVTNo0rU9BaJ0EMFdWw2+5HzctViMSCJMYAc=-VvDyLa9c1lGR8IJXj/hIAkftDOXOhjNsBEbSVA3qFtg=-PxUJ5HY;N7hKxUNu",
   environment: "TEST",
 });
