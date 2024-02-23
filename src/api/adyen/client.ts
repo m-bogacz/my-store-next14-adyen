dff3a068948dbf8e87044fa20b752432f62f120b
mateudev <mateuszbogacz.dev@gmail.com>
2024-02-23 11:27:19 +0100

    Add client key

--- src/api/adyen/client.ts
+++ src/api/adyen/client.ts
@@ -1,6 +1,6 @@
 import { Client } from "@adyen/api-library";
 
 export const clientAdyen = new Client({
-  apiKey: process.env.API_KEY_ADYEN ?? "",
+  apiKey: process.env.API_KEY_ADYEN ?? "test_EXV25GBNCZDHRCZSGIZG7EORRMMACP46",
   environment: "TEST",
 });
