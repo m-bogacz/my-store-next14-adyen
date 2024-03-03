cd1b887c5683d0488bdf87fad3629e591fcb370b
mateudev <mateuszbogacz.dev@gmail.com>
2024-03-03 18:24:34 +0100

    fix: cookie middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,17 +2,15 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const cookie = request.cookies.get("cartId");
-
-  if (!cookie) {
-    return NextResponse.next();
-  }
-  const response = NextResponse.next();
-
-  response.cookies.set({
-    name: cookie?.name,
-    value: cookie?.value,
-    httpOnly: true,
-  });
-  return response;
+  // const cookie = request.cookies.get("cartId");
+  // if (!cookie) {
+  //   return NextResponse.next();
+  // }
+  // const response = NextResponse.next();
+  // response.cookies.set({
+  //   name: cookie?.name,
+  //   value: cookie?.value,
+  //   httpOnly: true,
+  // });
+  // return response;
 }
