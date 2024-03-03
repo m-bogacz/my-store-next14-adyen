c3cc522e677a3a1cd7e239779a794f414ceb11dd
mateudev <mateuszbogacz.dev@gmail.com>
2024-03-03 14:02:57 +0100

    test: webhook

--- src/app/api/adyen/webhooks/notifications/route.ts
+++ src/app/api/adyen/webhooks/notifications/route.ts
@@ -1,13 +1,14 @@
-import { env } from "@/env";
-import { hmacValidator } from "@adyen/api-library";
+// import { env } from "@/env";
+// import { hmacValidator } from "@adyen/api-library";
+import { NextRequest } from "next/server";
 
-export async function POST(request: Request) {
-  const hmacKey = env.HMAC_VERIFY_KEY;
-  const validator = new hmacValidator();
+export async function POST(request: NextRequest) {
+  // const hmacKey = env.HMAC_VERIFY_KEY;
+  // const validator = new hmacValidator();
 
-  console.log("validator", validator);
-  console.log("hmacKey", hmacKey);
+  // console.log("validator", validator);
+  // console.log("hmacKey", hmacKey);
 
-  console.log("request", request);
+  console.log("request", request.body);
   return Response.json({ status: 200 });
 }
